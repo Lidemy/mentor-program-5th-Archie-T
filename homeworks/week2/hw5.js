8dccb5793f7f90460c00f5a06d4301037367c643
Archie-T <T.levitating@gmail.com>
2021-07-30 14:44:03 +0800

    5f

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,22 @@
 function join(arr, concatStr) {
-  
+  let ans = ''
+  for (let i = 0; i < arr.length; i++) {
+    if (i === arr.length - 1) {
+      ans += arr[i]
+    } else {
+      ans += arr[i] + concatStr
+    }
+  }
+  return ans
 }
 
 function repeat(str, times) {
-  
+  let ans = ''
+  for (let i = 1; i <= times; i++) {
+    ans += str
+  }
+  return ans
 }
 
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+console.log(join(['a', 'a', 'a'], '!'))
+console.log(repeat('a', 5))
