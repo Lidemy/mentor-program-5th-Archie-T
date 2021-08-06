c817da7c17e698e4240436fa7d44eadde26e19b7
Archie-T <T.levitating@gmail.com>
2021-08-06 16:29:19 +0800

    1f

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,3 +1,16 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin
+})
+
+const lines = []
+
+// 讀取到一行，先把這一行加進去 lines 陣列，最後再一起處理
+rl.on('line', (line) => {
+  lines.push(line)
+})
+
 function solve(lines) {
   let stars = ''
   if (lines >= 1 && lines <= 30) {
@@ -8,4 +21,7 @@ function solve(lines) {
   }
 }
 
-solve(11)
+// 輸入結束，開始針對 lines 做處理
+rl.on('close', () => {
+  solve(lines)
+})
